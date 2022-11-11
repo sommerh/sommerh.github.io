61c9ea493fe2f88dd3af95faac747a61dc138d08
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-11 09:07:59 -0500

    Start working in app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg'; //put photo into src folder and import
+// import './App.css'; //all css in index, use styled components
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Nav --don't show on home */}
+      {/* Home */}
+      {/* AboutMe */}
+      {/* Projects */}
+      {/* Skills */}
+      {/* ContactMe */}
     </div>
   );
 }
 
 export default App;
+
+//maybe keep some kind of state that goes to true when component is anything but home.
+//make a useEffect that checks when component changes, and adds or deletes nav
+
+//leaving this hear to review code tag below
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
