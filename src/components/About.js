59c15abd1d18e01e488db0a334c5133c7a6f18f6
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-16 14:31:28 -0500

    Add About template

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBar from './NavBar';
+import headshot from '../headshot.jpg';
 
 const About = () => {
   return (
     <div id="about" className="segment">
-      <NavBar />
-      <h1>This is about me</h1>
+      <img src={headshot} id="headshot" alt="hiking-headshot" />
+      <div>
+        <p>hello</p>
+        <p>more text</p>
+      </div>
     </div>
   );
 };
