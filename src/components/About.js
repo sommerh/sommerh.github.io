2b551e4b16a755863ff87c702fabd0ff29fe02e9
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-16 19:42:50 -0500

    Add about me section

--- src/components/About.js
+++ src/components/About.js
@@ -6,9 +6,19 @@ const About = () => {
     <div id="about" className="segment">
       <div id="about-body">
         <img src={headshot} id="headshot" alt="hiking-headshot" />
-        <div>
-          <p>hello</p>
-          <p>more text</p>
+        <div id="about-text">
+          <p>ABOUT ME</p>
+          <p>{`I’m a public health social worker and program manger-turned-software engineer based in New York City. Prior to my shift into this industry, I worked in a healthcare setting with inviduals who have experienced crime. I loved the work I was doing, but found myself being pulled in another direction as I saw the profound impact tech could have on an individual’s wellbeing and quality-of-life.`}</p>
+          <p>{`In social work, I learned how to use my set of skills to solve difficult constantly changing problems. I worked with interdisciplinary contacts and thought creatively to find the best solutions for my clients. As a program manager, I supervised and supported the team, managed and facilitated volunteer trainings, and kept a high caseload of my own.`}</p>
+          <p>
+            {`In Fullstack Academy’s Grace Hopper Program, I learned to build projects from ideation to deployment while implementing Agile methods, involving high levels of `}
+            <b>communication and collaboration</b>
+            {`. I enjoyed transferring my`}
+            <b>problem-solving, creativity, and mangement skills</b>
+            {`problem-solving, creativity, and mangement skills to this new
+            environment. I am excited to contribute to a team at a
+            purpose-driven company, where I can bring this all together.`}
+          </p>
         </div>
       </div>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div id="navbar">
+    <nav id="navbar">
       <a href="#home">{'</> HANNAH SOMMER'}</a>
       <a href="#about">About</a>
       <a href="#projects">Projects</a>
       <a href="#skills">Skills</a>
       <a href="#contact">Contact</a>
-    </div>
+    </nav>
   );
 };
 
