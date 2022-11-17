96825d9e85fbb6678885ccfbd84c7fb6e3052fd8
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-16 21:36:49 -0500

    Add projects template;

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Projects = () => {
   return (
     <div id="projects" className="segment">
-      <h1>This is projects</h1>
+      <div id="projects-body">
+        <h1>Projects</h1>
+        <div id="project-icons">
+          <div>project1</div>
+          <div>project2</div>
+          <div>project3</div>
+          <div>project4</div>
+          <div>project5</div>
+        </div>
+      </div>
     </div>
   );
 };
