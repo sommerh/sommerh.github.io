abef0dcc5ed21a798e29fd983f63afd5c8ccd8e9
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-19 15:15:31 -0500

    Add front view and styling of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react';
 // import creatureCoders from '../images/creature-coders.jpg';
-import creatureCodersTwo from '../images/creature-coders-2.jpg';
+import creatureCoders from '../images/creature-coders-border.jpg';
+import worx from '../images/bells-custom-worx.jpg';
+import wawdt from '../images/wawdt.jpg';
+import inhale from '../images/inhale.jpg';
 
 /*
 TODO:
@@ -16,22 +19,57 @@ const Projects = () => {
         <h1 className="font-link">Projects</h1>
         <div id="project-icons">
           <div className="project">
-            {/* <div className="project-top"> */}
-            <img
-              src={creatureCodersTwo}
-              className="project-img"
-              alt="creature-coders"
-            />
-            {/* </div> */}
+            <div className="img-container">
+              <img
+                src={creatureCoders}
+                className="project-img"
+                alt="creature-coders"
+              />
+            </div>
             <div className="project-bottom">
               <p className="font-link">Creature Coders</p>
               <p>A mobile-first learn-to-code game</p>
             </div>
           </div>
-          <div>project2</div>
-          <div>project3</div>
-          <div>project4</div>
-          <div>project5</div>
+          <div className="project">
+            <div className="img-container">
+              <img
+                src={wawdt}
+                className="project-img"
+                alt="what-are-we-doing-today"
+              />
+            </div>
+            <div className="project-bottom">
+              <p className="font-link">What Are We Doing Today?</p>
+              <p>
+                A decision-making app to get users out of the house and into the
+                community
+              </p>
+            </div>
+          </div>
+          <div className="project">
+            <div className="img-container">
+              <img src={worx} className="project-img" alt="bells-custom-worx" />
+            </div>
+            <div className="project-bottom">
+              <p className="font-link">Bell's Custom Worx</p>
+              <p>
+                An e-commerce site for selling handcrafted decor and furnishings
+              </p>
+            </div>
+          </div>
+          <div className="project">
+            <div className="img-container">
+              <img src={inhale} className="project-img" alt="inhale" />
+            </div>
+            <div className="project-bottom">
+              <p className="font-link">inhale</p>
+              <p>
+                An interactive wellness site focused on breathing/grounding
+                strategies
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
