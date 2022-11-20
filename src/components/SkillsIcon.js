ce5a6a2af31a1d3a1ece1138cac77c6e4710183f
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-19 19:41:35 -0500

    Add skills icon component template

---
+++ src/components/SkillsIcon.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SkillsIcon = () => {
+  return (
+    <div className="icon-group">
+      <img src={javascript} className="icons" alt="javascript" />
+      <p>Javascript</p>
+    </div>
+  );
+};
+
+export default SkillsIcon;
