2171e441d8cfb9d9ca83781c1f1f322aca5c3545
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-23 13:57:10 -0500

    Working hamburger icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //TODO: make the section user clicks turn pink (window.location.href)
 //OR
 //make name pink and when user hovers increase size
 
 const NavBar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   return (
     <nav id="navbar" className="font-link teal-bg">
       <a href="/" className="pink-txt-lt">
         {'</> HANNAH SOMMER'}
       </a>
-      <button className="hamburger">
-        {/* icon from heroicons.com */}
+      <button
+        className="hamburger"
+        onClick={() => setIsNavExpanded(!isNavExpanded)}
+      >
+        {/* <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+          />
+        </svg> */}
+
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -27,19 +46,32 @@ const NavBar = () => {
           />
         </svg>
       </button>
-      <div id="nav-menu">
-        <a href="#about" className="menu white-txt">
-          About
-        </a>
-        <a href="#projects" className="menu white-txt">
-          Projects
-        </a>
-        <a href="#skills" className="menu white-txt">
-          Skills
-        </a>
-        <a href="#contact" className="menu white-txt">
-          Contact
-        </a>
+      <div
+        className={isNavExpanded ? 'nav-menu expanded' : 'nav-menu'}
+        onClick={() => setIsNavExpanded(!isNavExpanded)}
+      >
+        <ul>
+          <li>
+            <a href="#about" className="menu white-txt">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#projects" className="menu white-txt">
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#skills" className="menu white-txt">
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="menu white-txt">
+              Contact
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
