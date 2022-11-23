423d85a9c2a5fa73d506c563e61599d41f5410b2
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-23 16:17:37 -0500

    Add responsive design

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,39 +9,27 @@ const NavBar = () => {
 
   return (
     <nav id="navbar" className="font-link teal-bg">
-      <a href="/" className="pink-txt-lt">
-        {'</> HANNAH SOMMER'}
+      <a href="/" className="pink-txt-lt nav-name">
+        {'<hs />'}
       </a>
+      {/* <a href="/" className="pink-txt-lt">
+        {'</> HANNAH SOMMER'}
+      </a> */}
       <button
         className="hamburger"
         onClick={() => setIsNavExpanded(!isNavExpanded)}
       >
-        {/* <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M6 18L18 6M6 6l12 12"
-          />
-        </svg> */}
-
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
-          class="w-6 h-6"
+          className="w-6 h-6"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           />
         </svg>
