a9182fa5362188dd4f2690b8998cd58d5b2f38a6
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-22 21:28:29 -0500

    Add and style hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,23 @@ const NavBar = () => {
       <a href="/" className="pink-txt-lt">
         {'</> HANNAH SOMMER'}
       </a>
+      <button className="hamburger">
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
       <a href="#about" className="white-txt">
         About
       </a>
