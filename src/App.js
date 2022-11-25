a9817070c2004f79c37b50d8ccafa0249725a17b
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-25 15:21:54 -0500

    Remove unneeded comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// import logo from './logo.svg'; //put photo into src folder and import
-// import './App.css'; //all css in index, use styled components
 import React from 'react';
 import About from './components/About';
 import Home from './components/Home';
@@ -24,25 +22,3 @@ function App() {
 }
 
 export default App;
-
-//leaving this hear to review code tag below
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
