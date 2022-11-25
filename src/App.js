79a4e4fb776c17817f1cbb1ed81a864533a2e4f7
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-25 12:57:38 -0500

    Add footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NavBar from './components/NavBar';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <em>designed & built by Hannah Sommer</em>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
